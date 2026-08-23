0c2acca18f88eaa31f9d9aafce2088e6b0e5c372
Hélder Rodrigues <me@shader.coffee>
2026-08-24 00:33:47 +0100

    nixfmt

--- home/common.nix
+++ home/common.nix
@@ -109,12 +109,12 @@
       RIPGREP_CONFIG_PATH = "${config.xdg.configHome}/ripgrep/rg.conf";
 
       PROMPT_COMMAND = lib.hm.nushell.mkNushellInline ''
-  {||
-    let pwd = ($env.PWD | str replace $env.HOME "~")
-    let host = (sys host | get hostname | split row "." | first)
-    $"(ansi cyan_bold)($env.USER)(ansi yellow_bold)@(ansi magenta_bold)($host)(ansi reset) (ansi red)($pwd)(ansi reset) "
-  }
-'';
+        {||
+          let pwd = ($env.PWD | str replace $env.HOME "~")
+          let host = (sys host | get hostname | split row "." | first)
+          $"(ansi cyan_bold)($env.USER)(ansi yellow_bold)@(ansi magenta_bold)($host)(ansi reset) (ansi red)($pwd)(ansi reset) "
+        }
+      '';
       PROMPT_INDICATOR = "";
       PROMPT_INDICATOR_VI_INSERT = "";
       PROMPT_INDICATOR_VI_NORMAL = ": ";
