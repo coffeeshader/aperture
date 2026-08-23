994df6cb11659e9fff6161937e3cfa7b842ba2ee
Hélder Rodrigues <me@shader.coffee>
2026-08-23 17:55:11 +0100

    plasma: add shortcuts for konsole and emacs

--- home/plasma.nix
+++ home/plasma.nix
@@ -11,6 +11,12 @@
       cursor.theme = "Breeze_Snow";
     };
 
+    shortcuts = {
+      "services/emacs.desktop"._launch = "Meta+E";
+      "services/org.kde.dolphin.desktop"._launch = [ ];
+      "services/org.kde.konsole.desktop"._launch = "Meta+Return";
+    };
+
     configFile = {
       kwinrc.Windows_HDR.MaxLuminance = 560;
       kwinrc.Windows_HDR.Reference = 250;
