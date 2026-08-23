b76fad2ebb6e78d745fdae8d60df8c944dc19fc4
Hélder Rodrigues <me@shader.coffee>
2026-08-23 20:30:52 +0100

    dev: setup codex + claude-code

--- home/development.nix
+++ home/development.nix
@@ -1,7 +1,6 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # ----- toolchains -----
     rustc
     cargo
     zig
@@ -17,7 +16,120 @@
     jdt-language-server
     nil
     nixfmt
+
+    bubblewrap
+    socat
   ];
 
   home.sessionVariables.JAVA_HOME = "${pkgs.jdk}/lib/openjdk";
+
+  programs.claude-code = {
+    enable = true;
+
+    settings = {
+      env = {
+        DISABLE_TELEMETRY = "1";
+        DISABLE_ERROR_REPORTING = "1";
+        CLAUDE_CODE_DISABLE_FEEDBACK_SURVEY = "1";
+
+        CLAUDE_CODE_SUBPROCESS_ENV_SCRUB = "1";
+      };
+
+      cleanupPeriodDays = 14;
+
+      attribution = {
+        commit = "";
+        pr = "";
+      };
+
+      permissions = {
+        defaultMode = "default";
+        disableBypassPermissionsMode = "disable";
+
+        ask = [
+          "Bash(dangerouslyDisableSandbox:true)"
+        ];
+
+        allow = [
+          "Bash(git status)"
+          "Bash(git diff *)"
+          "Bash(git log *)"
+        ];
+
+        deny = [
+          "Read(./.env)"
+          "Read(./.env.*)"
+          "Read(~/.ssh/**)"
+          "Read(~/.cargo/credentials)"
+          "Read(~/.cargo/credentials.toml)"
+        ];
+      };
+
+      sandbox = {
+        enabled = true;
+        failIfUnavailable = true;
+        allowUnsandboxedCommands = true;
+
+        filesystem.allowWrite = [
+          "${config.home.homeDirectory}/.cargo"
+          "${config.home.homeDirectory}/.m2/repository"
+          "${config.home.homeDirectory}/.cache/zig"
+        ];
+
+        network.allowedDomains = [
+          "github.com"
+          "api.github.com"
+          "raw.githubusercontent.com"
+          "codeload.github.com"
+          "objects.githubusercontent.com"
+          "release-assets.githubusercontent.com"
+
+          "codeberg.org"
+          "git.sr.ht"
+
+          "cache.nixos.org"
+          "channels.nixos.org"
+          "releases.nixos.org"
+          "search.nixos.org"
+
+          "crates.io"
+          "static.crates.io"
+          "index.crates.io"
+          "docs.rs"
+
+          "ziglang.org"
+
+          "repo.maven.apache.org"
+          "repo1.maven.org"
+
+          "developer.mozilla.org"
+        ];
+      };
+    };
+  };
+
+  programs.codex = {
+    enable = true;
+
+    settings = {
+      approval_policy = "on-request";
+      sandbox_mode = "workspace-write";
+
+      analytics.enabled = false;
+      feedback.enabled = false;
+
+      sandbox_workspace_write.writable_roots = [
+        "${config.home.homeDirectory}/.cargo"
+        "${config.home.homeDirectory}/.m2/repository"
+        "${config.home.homeDirectory}/.cache/zig"
+      ];
+
+      otel = {
+        exporter = "none";
+        metrics_exporter = "none";
+        trace_exporter = "none";
+        log_user_prompt = false;
+      };
+    };
+  };
 }
