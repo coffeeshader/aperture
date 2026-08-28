22b846bb02376cd381fe396f7d7936086f200def
Hélder Rodrigues <me@shader.coffee>
2026-08-28 22:10:16 +0100

    audio: follow the source sample rate

--- modules/audio.nix
+++ modules/audio.nix
@@ -8,5 +8,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire."10-allowed-rates"."context.properties"."default.clock.allowed-rates" = [
+      44100
+      48000
+      88200
+      96000
+      176400
+      192000
+    ];
   };
 }
