d625956524df5274ce20b211a6e7bfb3880a93bf
Hélder Rodrigues <me@shader.coffee>
2026-09-02 18:56:04 +0100

    login: fix kwallet unlock

--- modules/desktop-hyprland.nix
+++ modules/desktop-hyprland.nix
@@ -13,6 +13,8 @@
 
   services.greetd = {
     enable = true;
+    useTextGreeter = true;
+
     settings.default_session.command = "${lib.getExe pkgs.tuigreet} --time --remember --remember-user-session --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
   };
 
@@ -20,9 +22,22 @@
 
   programs.ssh.askPassword = lib.getExe pkgs.kdePackages.ksshaskpass;
 
-  security.pam.services.greetd.kwallet = {
+  security.pam.services.login.kwallet = {
     enable = true;
     forceRun = true;
+  };
+
+  systemd.user.services.plasma-kwallet-pam = {
+    description = "Unlock kwallet from pam credentials";
+    partOf = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.kdePackages.kwallet-pam}/libexec/pam_kwallet_init";
+      Type = "simple";
+      Slice = "background.slice";
+      Restart = "no";
+    };
   };
 
   environment.systemPackages = [
