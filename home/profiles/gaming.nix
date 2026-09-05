6d818d1c30337833fe4f093b643f4d14c0c216c2
Hélder Rodrigues <me@shader.coffee>
2026-09-05 16:28:55 +0100

    gaming: fix mangohud config being overriden by catppuccin theme

--- home/profiles/gaming.nix
+++ home/profiles/gaming.nix
@@ -6,15 +6,20 @@
     enableSessionWide = true;
   };
 
+  catppuccin.mangohud.enable = false;
+
   xdg.configFile."MangoHud/MangoHud.conf".text = ''
     control=mangohud
     legacy_layout=0
     horizontal
     hud_no_margin
+    horizontal_stretch=0
     table_columns=14
+    font_size=18
+    font_scale_media_player=1.0
 
     fps
-    frametime=0
+    frametime=1
     frame_timing=1
     gpu_stats
     gpu_power
